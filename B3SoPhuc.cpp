43a6472ed9c3df72056cc1532938e523f328f6bc
hstung12a1@gmail.com <hstung12a1@gmail.com>
2022-03-29 15:05:14 +0700

    Tong So Phuc

---
+++ B3SoPhuc.cpp
@@ -0,0 +1,58 @@
+#include<iostream> 
+
+using namespace std; 
+
+
+
+class SoPhuc {
+    private: 
+            int thuc; 
+            int ao; 
+    public : 
+            SoPhuc(int a = 0 , int b = 0 ) {
+                thuc = a; 
+                ao = b; 
+            }
+            ~SoPhuc(){} 
+
+            void outPut(){
+                cout<< "\nSo Phuc: " << thuc ; 
+                if( ao < 0 ){
+                    cout<< ao << "*j"; 
+                }
+                else {
+                    cout<< " + " << ao << "*j" ; 
+                }
+            }
+
+            int getThuc(){
+                return thuc; 
+            }
+            int getAo(){
+                return ao; 
+            }
+
+            SoPhuc tong2SP(SoPhuc p1){
+                int e , g, h ; 
+                e = thuc + p1.getThuc(); 
+                g = ao + p1.getAo(); 
+                SoPhuc sp(e,g); 
+                return sp; 
+            }
+
+}; 
+
+
+
+int main(){
+
+    SoPhuc P1(4,6); 
+    SoPhuc P2(2,8); 
+
+    SoPhuc P = P2.tong2SP(P1); 
+    P.outPut(); 
+
+
+
+    return 0; 
+}
